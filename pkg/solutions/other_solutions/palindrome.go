391c0c570291b58c178908b99786df507187a19d
Nikhod Tursunzoda <tursunzodanikha@gmail.com>
2025-01-24 17:15:43 +0500

    new solution: is palindrom

---
+++ pkg/solutions/other_solutions/palindrome.go
@@ -0,0 +1,30 @@
+package other_solutions
+
+import "strings"
+
+func IsPalindrom(text string) bool {
+	text = strings.ToLower(text)
+
+	text = RemoveSpace(text)
+	reverseCounter := len(text) - 1
+
+	for i := 0; i < len(text); i++ {
+		if text[i] != text[reverseCounter] {
+			return false
+		}
+		reverseCounter--
+	}
+
+	return true
+}
+
+func RemoveSpace(text string) string {
+	var clearSlice []uint8
+	for i := 0; i < len(text); i++ {
+		if char := text[i]; rune(char) != ' ' {
+			clearSlice = append(clearSlice, char)
+		}
+	}
+
+	return string(clearSlice)
+}
