1b0414424e8837b4150f960da3bef62a91cae545
Nikhod Tursunzoda <tursunzodanikha@gmail.com>
2025-02-05 18:33:02 +0500

    mistakes were fixed

--- pkg/solutions/recursion/find_amount_of_number_usage.go
+++ pkg/solutions/recursion/find_amount_of_number_usage.go
@@ -4,20 +4,20 @@ import "fmt"
 
 func FindAmountOfNumberEntry() {
 	arr := []int{23, 77, 44, 77, 56, 666, 78, 77, 88, 9999, 777, 77}
-	usage := FindUsage(arr, 77, 0)
+	usage := FindUsage(arr, 77, 0, 0)
 	fmt.Println(usage)
 }
 
-func FindUsage(arr []int, number, index int) int {
-	var counter int
+func FindUsage(arr []int, number, index, counter int) int {
 
 	if index >= len(arr) {
-		return -1
-	}
-
-	if number == arr[index] {
 		return counter
 	}
 
-	return FindUsage(arr, number, index+1)
+	if number == arr[index] {
+		counter++
+		return FindUsage(arr, number, index+1, counter)
+	}
+
+	return FindUsage(arr, number, index+1, counter)
 }
