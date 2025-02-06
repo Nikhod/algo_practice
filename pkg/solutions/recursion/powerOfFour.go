d0cef05aae183dab337796eafcdcd679be24fce0
Nikhod Tursunzoda <tursunzodanikha@gmail.com>
2025-02-06 15:44:11 +0500

    new recursion task

---
+++ pkg/solutions/recursion/powerOfFour.go
@@ -0,0 +1,16 @@
+package recursion
+
+func isPowerOfFour(n int) bool {
+	if n == 1 {
+		return true
+	}
+
+	if n <= 0 {
+		return false
+	}
+
+	if n%4 == 0 {
+		isPowerOfFour(n / 4)
+	}
+	return false
+}
