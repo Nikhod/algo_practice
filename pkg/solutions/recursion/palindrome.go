2581818d4e5568ef8e7de4bf4266132020eaefc2
Nikhod Tursunzoda <tursunzodanikha@gmail.com>
2025-02-08 16:26:42 +0500

    palindrome by recursion

--- pkg/solutions/recursion/palindrome.go
+++ pkg/solutions/recursion/palindrome.go
@@ -1,1 +1,14 @@
 package recursion
+
+// one can optimize this func by using the  array of rune or byte (uint8)
+func IsPalindrome(text []byte) bool {
+	if len(text) == 1 || len(text) == 0 {
+		return true
+	}
+	if text[0] == text[len(text)-1] {
+		text = text[1 : len(text)-1]
+		return IsPalindrome(text)
+	}
+
+	return false
+}
