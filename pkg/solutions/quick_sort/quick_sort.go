08062ba43a0f4d2962090587b03126ba85a3a5b7
Nikhod Tursunzoda <tursunzodanikha@gmail.com>
2025-02-09 20:59:41 +0500

    added new algo-practice: quick sort

--- pkg/solutions/quick_sort/quick_sort.go
+++ pkg/solutions/quick_sort/quick_sort.go
@@ -1,1 +1,32 @@
 package quick_sort
+
+func Sort(inputArr []int) []int {
+	if len(inputArr) <= 1 {
+		return inputArr
+	} else {
+		//	sequence:
+		//	less -> pivot -> greater
+		pivot := inputArr[0]
+		less, greater := divideSlice(inputArr, pivot)
+
+		less = Sort(less)
+		greater = Sort(greater)
+
+		less = append(less, pivot)
+		less = append(less, greater...)
+		return less
+	}
+}
+
+// divideSlice: divide the slice to less than pivot and greater than pivot
+func divideSlice(inputArr []int, pivot int) (less, greater []int) {
+	for i := 1; i < len(inputArr); i++ {
+		iterable := inputArr[i]
+		if iterable > pivot {
+			greater = append(greater, iterable)
+		} else {
+			less = append(less, iterable)
+		}
+	}
+	return
+}
