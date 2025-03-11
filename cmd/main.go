7e143c149404b4fda2d2ba5fe825919c25e99fde
Nihod Tursunzoda <NihodT@HUMO.local>
2025-03-11 19:22:47 +0500

    one must finish at home

--- cmd/main.go
+++ cmd/main.go
@@ -1,9 +1,15 @@
 package main
 
-import "algo/pkg/data_structure"
+import (
+	"algo/pkg/solutions/binary_search"
+	"fmt"
+)
 
 func main() {
-	//data_structure.PrintGraphUsingMechanicWay()
-	//data_structure.PrintGraphUsingMethods()
-	data_structure.CorrectPrintingGraph()
+	nums := []int{5, 7, 7, 8, 8, 10}
+	target := 8
+
+	first, last := binary_search.ThirdFindFirstAndLastEntry(nums, target)
+	//first, last := FindFirstAndLastEntry(nums, target)
+	fmt.Println(first, last)
 }
