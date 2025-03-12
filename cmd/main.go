f1631d967f15dbec4ecb13c74450ceba53807478
NihodT <tursunzodanikha@gmail.com>
2025-03-12 13:46:11 +0500

    return go algo in main file

--- cmd/main.go
+++ cmd/main.go
@@ -1,15 +1,7 @@
 package main
 
-import (
-	"algo/pkg/solutions/binary_search"
-	"fmt"
-)
+import "fmt"
 
 func main() {
-	nums := []int{5, 7, 7, 8, 8, 10}
-	target := 8
-
-	first, last := binary_search.ThirdFindFirstAndLastEntry(nums, target)
-	//first, last := FindFirstAndLastEntry(nums, target)
-	fmt.Println(first, last)
+	fmt.Println("go algo")
 }
