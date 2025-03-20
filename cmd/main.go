570de3b82373fc01b71030fc7be7344776591dc0
NihodT <tursunzodanikha@gmail.com>
2025-03-20 20:59:16 +0500

    home

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,15 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
 	fmt.Println("go algo")
+	//nums1 := []int{1, 2, 3, 6}
+	//nums2 := []int{2, 3, 4, 5}
+	nums1 := []int{6, 13, 18, 18, 28, 34, 37, 39, 46, 50, 52, 54, 62, 63, 65, 66, 75, 80, 97, 98}
+	nums2 := []int{10, 13, 13, 19, 27, 33, 40, 41, 43, 46, 56, 61, 69, 72, 78, 79, 82, 88, 91, 94}
+
+	fmt.Println(getCommon(nums1, nums2))
 }
--- pkg/solutions/binary_search/minimum_value_of_two_array.go
+++ pkg/solutions/binary_search/minimum_value_of_two_array.go
@@ -1,1 +1,35 @@
 package binary_search
+
+// leetCode: 2540
+// Даны два целочисленных массива nums1и nums2, отсортированные в неубывающем порядке, вернуть минимальное целое число,
+// общее для обоих массивов nums1 . Если общего целого числа среди и нет nums2, вернуть -1.
+// Обратите внимание, что целое число считается общим для nums1, nums2если в обоих массивах есть хотя бы одно вхождение
+// этого целого числа.
+func GetCommon(nums1 []int, nums2 []int) int {
+	var minimum int = -1
+	// todo O(n log n) - линейно пройдись по 1 массиву, и каждый его элемент находи во втором массиве.
+	// todo первое вхождение - будет ответом
+
+	return minimum
+}
+
+func getCommon(nums1 []int, nums2 []int) int {
+	minimum := -1
+
+	if len(nums1) == 0 || len(nums2) == 0 {
+		return -1
+	}
+
+	var firstPointer, secondPointer int = 0, 0
+	for firstPointer < len(nums1) && secondPointer < len(nums2) {
+		if nums1[firstPointer] == nums2[secondPointer] {
+			minimum = nums1[firstPointer]
+			return minimum
+		} else if nums1[firstPointer] < nums2[secondPointer] {
+			firstPointer++
+		} else if nums1[firstPointer] > nums2[secondPointer] {
+			secondPointer++
+		}
+	}
+	return minimum
+}
